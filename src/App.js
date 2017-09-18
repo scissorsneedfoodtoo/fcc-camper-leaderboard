34acc0fd61be14b56fecb7b106b3f1d705d6330b
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2017-09-18 18:26:03 +0900

    found and fixed a bug that switched the leaderboards if the user happened to click the span/caret. Changed it so that it toggles the ascending/decending order of the scores. cleaned up and fixed some of the css

--- src/App.js
+++ src/App.js
@@ -22,14 +22,15 @@ class App extends Component {
     const otherButtonKey = e.target.innerText === 'Recent' ? 'topActive' : 'recentActive'
     const decending = this.state.decending
     const currentLeaderboard = this.state.recentActive ? this.state.recentLeaderboard : this.state.topLeaderboard
+    const isCaret = e.target.innerText.length === 0 // check if user clicks empty span for caret
 
-    if (isInactive && decending) { // ex: displaying recent scores in decending order and click top score
+    if (isInactive && decending && !isCaret) { // ex: displaying recent scores in decending order and click top score
       this.setState({
         [thisButtonKey]: true,
         [otherButtonKey]: false,
         decending: true
       })
-    } else if (isInactive && !decending) { // ex: displaying recent scores in ascending order and click top score
+    } else if (isInactive && !decending && !isCaret) { // ex: displaying recent scores in ascending order and click top score
       // reverse whatever the current leaderboard is (in this ex, the recent
       // scores leaderboard) so that it's back to default decending order
       currentLeaderboard.reverse()
@@ -39,7 +40,7 @@ class App extends Component {
         [otherButtonKey]: false,
         decending: true
       })
-    } else if (!isInactive) { // ex: displaying recent scores and click recent scores button again
+    } else if (!isInactive || isCaret) { // ex: displaying recent scores and click recent scores button again
       // reverse whatever the current leaderboard is (in this ex, the recent
       // scores leaderboard) to display it in ascending or decending order
       currentLeaderboard.reverse()
@@ -97,49 +98,52 @@ class App extends Component {
 
     return (
       <div className="App">
-        <div className="App-header">
-          <div className="logo">
-            <a href="https://www.freecodecamp.com">
-              <img src="https://res.cloudinary.com/scissorsneedfoodtoo/image/upload/q_100/v1505302849/freecodecamp_logo.svg"
-              alt="FreeCodeCamp logo"/>
-            </a>
+        <div className="content">
+          <div className="App-header">
+            <div className="logo">
+              <a href="https://www.freecodecamp.com">
+                <img src="https://res.cloudinary.com/scissorsneedfoodtoo/image/upload/q_100/v1505302849/freecodecamp_logo.svg"
+                alt="FreeCodeCamp logo"/>
+              </a>
+            </div>
+            {/* end logo */}
+            <h2>Camper Leaderboard</h2>
           </div>
-          {/* end logo */}
-          <h2>Camper Leaderboard</h2>
-        </div>
-        {/* end App-header */}
-        <div className="button-bar">
-          <button className={`button recent ${this.toggleClass(this.state.recentActive)}`} onClick={(event) => this.handleClick(event)}>Recent<span className={`fa ${this.toggleCaret(this.state.recentActive)}`} /></button>
-          <button className={`button top ${this.toggleClass(this.state.topActive)}`} onClick={(event) => this.handleClick(event)}>Top Score<span className={`fa ${this.toggleCaret(this.state.topActive)}`} /></button>
-        </div>
-        {/* end button-bar */}
-        <div className="scoreboard">
-          {targetLeaderboard.map(function(obj, index) {
-            const points = recent ? obj.recent : obj.alltime
-            return (
-              <div className="score-container" key={index}>
-                <div className={`score ${index}`}>
-                  <a href={`https://www.freecodecamp.org/${obj.username}`}>
-                    <div className="rank">
-                      <p>{index + 1}</p>
-                    </div>
-                    <div className="avatar">
-                      <img src={obj.img} alt={`${obj.username}'s' avatar`}/>
-                    </div>
-                    <div className="username">
-                      <p>{obj.username}</p>
-                    </div>
-                    <div className="points">
-                      <p>{points}</p>
-                    </div>
-                  </a>
+          {/* end App-header */}
+          <div className="button-bar">
+            <button className={`button recent ${this.toggleClass(this.state.recentActive)}`} onClick={(event) => this.handleClick(event)}>Recent<span className={`fa ${this.toggleCaret(this.state.recentActive)}`} /></button>
+            <button className={`button top ${this.toggleClass(this.state.topActive)}`} onClick={(event) => this.handleClick(event)}>Top Score<span className={`fa ${this.toggleCaret(this.state.topActive)}`} /></button>
+          </div>
+          {/* end button-bar */}
+          <div className="scoreboard">
+            {targetLeaderboard.map(function(obj, index) {
+              const points = recent ? obj.recent : obj.alltime
+              return (
+                <div className="score-container" key={index}>
+                  <div className={`score ${index}`}>
+                    <a href={`https://www.freecodecamp.org/${obj.username}`}>
+                      <div className="rank">
+                        <p>{index + 1}</p>
+                      </div>
+                      <div className="avatar">
+                        <img src={obj.img} alt={`${obj.username}'s' avatar`}/>
+                      </div>
+                      <div className="username">
+                        <p>{obj.username}</p>
+                      </div>
+                      <div className="points">
+                        <p>{points}</p>
+                      </div>
+                    </a>
+                  </div>
+                  {index < 99 ? <hr /> : ""}
                 </div>
-                {index < 99 ? <hr /> : ""}
-              </div>
-            )
-          })}
+              )
+            })}
+          </div>
+          {/* end scoreboard */}
         </div>
-        {/* end scoreboard */}
+        {/* end content */}
         <div className="footer" />
       </div> // end App
     );
